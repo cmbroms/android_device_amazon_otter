32ac756e63f2d71317a80e39a4478a74ded4a056
nycbjr <nycbjr@gmail.com>
2014-01-23 15:43:09 -0500

    fixing otter lunch

--- Android.mk
+++ Android.mk
@@ -18,6 +18,6 @@ ifeq ($(TARGET_DEVICE),otter)
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(TARGET_SIMULATOR),true)
-include $(call all-makefiles-under,$(LOCAL_PATH))
+include $(call first-makefiles-under,$(LOCAL_PATH))
 endif
 endif
